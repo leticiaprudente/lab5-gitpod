0eeeb75e7e9ed0309dca79587659287d7434ddc2
leticiaprudente <let.prudente@gmail.com>
2020-10-09 02:10:56 +0000

    correção findByNome Fornecedor

--- src/main/java/br/gov/sp/fatec/lab5/repository/FornecedorRepository.java
+++ src/main/java/br/gov/sp/fatec/lab5/repository/FornecedorRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FornecedorRepository extends CrudRepository<Fornecedor, Long> {
-    Fornecedor findBymvNome(String nome);
+    Fornecedor findByNome(String nome);
 }
