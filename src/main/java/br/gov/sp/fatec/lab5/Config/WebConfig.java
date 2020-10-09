3bef790ae55b7838002b483bab3d590c852913eb
leticiaprudente <let.prudente@gmail.com>
2020-10-09 02:39:02 +0000

    correção webconfig

--- src/main/java/br/gov/sp/fatec/lab5/Config/WebConfig.java
+++ src/main/java/br/gov/sp/fatec/lab5/Config/WebConfig.java
@@ -1,8 +1,13 @@
 package br.gov.sp.fatec.lab5.Config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+@Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
